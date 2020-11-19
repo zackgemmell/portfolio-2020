f3d7c967b76fa8921890cf5a7a4c550ebbed1d90
Misha <Rubon72@gmail.com>
2020-11-19 23:04:30 +0300

    header links issue

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
 
 // Add smooth scrolling to all links
-  $("nav > a").on('click', function(event) {
+  $("#header-home > a").on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
